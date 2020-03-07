a4830b038cd3091b3b80b8bff189c9d4626c0fc9
bahilo <sisi.bahilo@gmail.com>
2020-03-07 16:56:23 +0100

    init

---
+++ src/Entity/Agent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * @ApiResource
+ * @ORM\Entity(repositoryClass="App\Repository\AgentRepository")
+ */
+class Agent
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    public $Name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
